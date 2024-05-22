2b5fb2edca7e57361d85b89763f66a4f3646ac71
Oliver Walsh <oliverwalsh45@gmail.com>
2024-05-21 19:24:03 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/* <div class="day-header">
+    <p>Thursday, May 23</p>
+</div> */
+
+/* <div class="task">
+    <div class="priority-marker high">HIGH</div>
+    <div class="task-title">Example Task</div>
+</div> */
+
+// task overview: task-container
+// task totals: reg-ps, etc.
